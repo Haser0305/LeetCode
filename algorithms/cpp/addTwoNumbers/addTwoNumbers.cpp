9cef397bee7dfa3a598ee6819b7f13e44476b340
Haser0305 <haser1156@gmail.com>
2022-06-29 21:51:00 +0800

    style: follow naming convention, also replace NULL to nullptr

--- algorithms/cpp/addTwoNumbers/addTwoNumbers.cpp
+++ algorithms/cpp/addTwoNumbers/addTwoNumbers.cpp
@@ -20,41 +20,43 @@
 
 #include "cstdio"
 
-struct ListNode {
+struct list_node {
     int val;
-    ListNode *next;
+    list_node* next;
 
-    ListNode() : val(0), next(nullptr) {}
+    list_node() : val(0), next(nullptr) {
+    }
 
-    ListNode(int x) : val(x), next(nullptr) {}
+    list_node(int x) : val(x), next(nullptr) {
+    }
 
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    list_node(int x, list_node* next) : val(x), next(next) {
+    }
 };
 
-ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-    ListNode *head = l1;
+list_node* add_two_numbers(list_node* l1, list_node* l2) {
+    list_node* head = l1;
     int carry = 0;
     int temp;
     while (true) {
         temp = l1->val;
         l1->val = (l1->val + l2->val + carry) % 10;
         carry = (temp + l2->val + carry) / 10;
-        if (l1->next == NULL && l2->next != NULL) {
-            l1->next = new ListNode();
-        } else if (l1->next != NULL && l2->next == NULL) {
-            l2->next = new ListNode();
+        if (l1->next == nullptr && l2->next != nullptr) {
+            l1->next = new list_node();
+        } else if (l1->next != nullptr && l2->next == nullptr) {
+            l2->next = new list_node();
         }
 
-        if (l1->next == NULL && l2->next == NULL) {
+        if (l1->next == nullptr && l2->next == nullptr) {
             break;
         } else {
             l1 = l1->next;
             l2 = l2->next;
         }
-
     }
     if (carry != 0) {
-        l1 = new ListNode(1);
+        l1 = new list_node(1);
     }
     return head;
 }
