a3742d71284101b1470fe3b702ff2726ab15df09
Haser0305 <haser1156@gmail.com>
2022-07-16 15:17:41 +0800

    add comments

--- algorithms/cpp/longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.cpp
+++ algorithms/cpp/longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.cpp
@@ -9,9 +9,9 @@
  * Given a string s, find the length of the longest substring without repeating characters.
  *
  * Example:
- * Input: s = "abcabcbb"
- * Output: 3
- * Explanation: The answer is "abc", with the length of 3.
+ *      Input: s = "abcabcbb"
+ *      Output: 3
+ *      Explanation: The answer is "abc", with the length of 3.
  *****************************/
 
 #include "iostream"
@@ -19,19 +19,19 @@
 
 using namespace std;
 
-int lengthOfLongestSubstring(string s) {
+int test(string s) {
     int n = s.size();
     int res = 0;
 
-    vector<int> lastIndex(256, -1);
+    vector<int> lastIndex(256, -1); // the size is 256, and all values are -1.
 
     int i = 0;
 
     for (int j = 0; j < n; j++) {
 
-        i = max(i, lastIndex[s[j]] + 1);
+        i = max(i, lastIndex[s[j]] + 1); // 用以決定起頭位置，如果遇到重複的就會往右重複數
 
-        res = max(res, j - i + 1);
+        res = max(res, j - i + 1); // 記錄當下字串的最長值，_Right:從左到右之間所有數的總和
 
         lastIndex[s[j]] = j;
     }
